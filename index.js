4a21fbf0bbb68750b575be81bb5cb3d0a43349d8
Kim Love <kimberlymlove15@gmail.com>
2023-08-06 16:58:54 -0400

    All tests passing

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  const modifiedArray = tutorials.map((sentence) => {
+    let newSentence = "";
+
+    for (let i = 0; i < sentence.length; i++) {
+      let letter = sentence[i];
+
+      if (i === 0) {
+        letter = letter.toUpperCase();
+        newSentence += letter;
+      } else if (sentence[i - 1] === " ") {
+        letter = letter.toUpperCase();
+        newSentence += letter;
+      } else {
+        newSentence += letter;
+      }
+    }
+
+    return newSentence
+  });
+
+  return modifiedArray;
+};
